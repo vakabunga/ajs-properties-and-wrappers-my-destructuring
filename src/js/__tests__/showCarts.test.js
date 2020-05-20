1dab69944669d3bad0a309623984b5280fc2d54f
Vakabunga <shesterov@gmail.com>
2020-05-20 07:34:38 +0300

    function done. todo: tests

---
+++ src/js/__tests__/showCarts.test.js
@@ -0,0 +1,24 @@
+// eslint-disable-next-line no-unused-vars
+import showCarts from '../showCarts';
+const person = {
+  name: 'Лучник',
+  type: 'Bowman',
+  health: 50,
+  level: 3,
+  attack: 40,
+  defence: 10,
+  special: [
+    {
+      id: 8,
+      name: 'Двойной выстрел',
+      icon: 'http://...',
+      description: 'Двойной выстрел наносит двойной урон',
+    },
+    {
+      id: 9,
+      name: 'Нокаутирующий удар',
+      icon: 'http://...',
+      // <- обратите внимание, описание "засекречено"
+    },
+  ],
+};
---
+++ src/js/showCarts.js
@@ -0,0 +1,17 @@
+
+export default function showCarts(unit) {
+  const { special } = unit;
+  const carts = [];
+  special.forEach((element) => {
+    const {
+      id, name, icon, description = 'not available yet',
+    } = element;
+    carts.push({
+      id,
+      name,
+      icon,
+      description,
+    });
+  });
+  return carts;
+}
